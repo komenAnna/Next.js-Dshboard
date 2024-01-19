0380f098a5ace9413a47cbce3a18a47c17e0ba46
komenAnna <komenriana@gmail.com>
2024-01-19 12:57:44 +0300

    Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Card } from '../ui/dashboard/cards'
+import RevenueChart from '../ui/dashboard/revenue-chart'
+import LatestInvoices from '../ui/dashboard/latest-invoices'
+import { lusitana } from '@/app/ui/fonts'
+import { revenue } from '../lib/placeholder-data'
+// To fetch data for the <RevenueChart/> component, import the fetchRevenue function from data.ts
+import { fetchRevenue } from '../lib/data'
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+
+export default async function Page() {
+  const revenue = fetchRevenue()
+  return(
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      {/* CARDS */}
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title='Collected' value={totalPaidInvoices} type='collected'/> */}
+        {/* <Card title='Pending' value={totalPendingInvoices} type='pending'/> */}
+        {/* <Card title='Total Invoices' value={numberOfInvoices} type='invoices'/> */}
+        {/* <Card title='Total Customers' value={numberOfCustomers} type='customers'/> */}
+      </div>
+      {/* CHARTS */}
+      <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+        <RevenueChart revenue={revenue}/>
+        {/* <LatestInvoices latestInvoices={latestInvoices}/> */}
+      </div>
+    </main>
   )
+ 
 }
-
-export default Dashboard
