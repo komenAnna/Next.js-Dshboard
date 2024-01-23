021be1d873aab3d737136f2d523111065dc42016
komenAnna <komenriana@gmail.com>
2024-01-23 12:45:48 +0300

    wrapped the <RevenueChart/> and <LatestInvoices/> components in Suspense, changed the data fetches for both to their respective components from the page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,12 +5,18 @@ import LatestInvoices from '../../ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
 import { revenue } from '../../lib/placeholder-data'
 // To fetch data for the <RevenueChart/> component, import the fetchRevenue function from data.ts
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../../lib/data'
+import {  fetchCardData } from '../../lib/data'
+//import {fetchRevenue, fetchLatestInvoices,} from '../../lib/data'
+//remove fetchRevenue, fetchLatestInvoices, and its instances from this file so as to use SUSPENSE to stream it as a component
+import { Suspense } from 'react'
+//import Suspense from react and wrap it around <RevenueChart/>
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons'
+// import the above as a fallback component for the revenue chart
 
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
+  // const revenue = await fetchRevenue()
+  // const latestInvoices = await fetchLatestInvoices()
   const {numberOfCustomers,
     numberOfInvoices,
     totalPaidInvoices,
@@ -27,8 +33,15 @@ export default async function Page() {
       </div>
       {/* CHARTS */}
       <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-        <RevenueChart revenue={revenue}/>
-        <LatestInvoices latestInvoices={latestInvoices}/>
+        {/* <RevenueChart revenue={revenue}/> */}
+        {/* replace the above with the Suspense Component */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        
       </div>
     </main>
   )
